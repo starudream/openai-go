020442697de8c251cc0d431a48fb9e1e42855f5e
starudream <justwangsheng@gmail.com>
2023-03-31 17:24:34 +0800

    feat: add client options

--- client_options.go
+++ client_options.go
@@ -1,10 +1,21 @@
 package openai
 
+import (
+	"github.com/go-resty/resty/v2"
+)
+
 type ClientOptionFunc func(c *Client) error
 
 func WithBaseURL(urlStr string) ClientOptionFunc {
 	return func(c *Client) error {
 		return c.setBaseURL(urlStr)
+	}
+}
+
+func WithClient(client *resty.Client) ClientOptionFunc {
+	return func(c *Client) error {
+		c.client = client
+		return nil
 	}
 }
 
